a58a1fe8967cca28c1902893a581142d920d079e
tachyon6 <tachyon@Tachyonui-MacBookPro.local>
2024-02-26 20:24:17 +0900

    gql 수정

--- src/gql/create-complete.js
+++ src/gql/create-complete.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_COMPLETE_FILE = gql`
-    mutation combine($complete_file_input: CompleteFileInput!) {
-        combine(complete_file_input: $complete_file_input)
+    mutation combine($complete_file_input: CompleteFileInput!, client_id: String!) {
+        combine(complete_file_input: $complete_file_input, client_id: client_id) {
     }
 `;
