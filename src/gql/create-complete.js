070fa851f4ee604cac7d3542b0ddeb4100398cbe
tachyon6 <tachyon@Tachyonui-MacBookPro.local>
2024-02-26 20:30:39 +0900

    gql error2

--- src/gql/create-complete.js
+++ src/gql/create-complete.js
@@ -2,6 +2,6 @@ import { gql } from "@apollo/client";
 
 export const CREATE_COMPLETE_FILE = gql`
     mutation combine($complete_file_input: CompleteFileInput!, $client_id: String!) {
-        combine(complete_file_input: $complete_file_input, client_id: $client_id) {
+        combine(complete_file_input: $complete_file_input, client_id: $client_id)
     }
 `;
