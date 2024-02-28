cebe175943602f0f144167766c8f6d5dcdf69601
tachyon6 <tachyon@Tachyonui-MacBookPro.local>
2024-02-28 22:04:51 +0900

    logo clickable

--- src/component/common/Header.jsx
+++ src/component/common/Header.jsx
@@ -25,14 +25,15 @@ const Header = () => {
 
   return (
     <HeaderContainer>
+      <div cursor="pointer" onClick={handleHomeClick}>
       <object type="image/svg+xml" data={logo}>
         <img
           src={logo}
           alt="logo"
           style={{ width: "8.5rem", height: "2.5rem", cursor: "pointer" }}
-          onClick={handleHomeClick}
         />
       </object>
+      </div>
       <NavContainer>
         <Nav onClick={handleHomeClick} active={isActive("/")}>
           기출문제 뽑기
